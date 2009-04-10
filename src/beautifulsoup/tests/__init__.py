8763e6146c553d1b950b8a1d35859b9b1bb7be6d
Leonard Richardson <leonard.richardson@canonical.com>
2009-04-10 11:48:02 -0400

    Removed autogenerated licensing information.

--- src/beautifulsoup/tests/__init__.py
+++ src/beautifulsoup/tests/__init__.py
@@ -1,16 +1,1 @@
-# Copyright 2009 Canonical Ltd.  All rights reserved.
-#
-# This file is part of beautifulsoup
-#
-# beautifulsoup is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# beautifulsoup is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with beautifulsoup.  If not, see <http://www.gnu.org/licenses/>.
 "The beautifulsoup tests."
