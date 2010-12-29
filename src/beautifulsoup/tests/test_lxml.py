4d4241a9ef27cfb08082d8388831893ff9f8ee94
Leonard Richardson <leonard.richardson@canonical.com>
2010-12-29 10:48:50 -0500

    Test suite cleanup.

--- src/beautifulsoup/tests/test_lxml.py
+++ src/beautifulsoup/tests/test_lxml.py
@@ -1,16 +1,8 @@
 from helpers import SoupTest
-from beautifulsoup import BeautifulSoup
-from beautifulsoup.element import SoupStrainer
 from beautifulsoup.builder.lxml_builder import LXMLTreeBuilder
-import unittest
+
 
 class TestLXMLBuilder(SoupTest):
-
-    def runTest(self):
-        self.test_bare_string()
-        self.test_tag_nesting()
-        self.test_self_closing()
-        self.test_soupstrainer()
 
     def test_bare_string(self):
         self.assertSoupEquals(
@@ -26,19 +18,3 @@ class TestLXMLBuilder(SoupTest):
     def test_self_closing(self):
         self.assertSoupEquals(
             "<p>A <meta> tag</p>", "<p>A <meta /> tag</p>")
-
-    def test_soupstrainer(self):
-        strainer = SoupStrainer("b")
-        soup = BeautifulSoup("A <b>bold</b> <i>statement</i>",
-                             self.default_builder,
-                             parseOnlyThese=strainer)
-        self.assertEquals(soup.decode(), "<b>bold</b>")
-
-        soup = BeautifulSoup("A <b>bold</b> <meta> <i>statement</i>",
-                             self.default_builder,
-                             parseOnlyThese=strainer)
-        self.assertEquals(soup.decode(), "<b>bold</b>")
-
-
-def test_suite():
-    return unittest.TestLoader().loadTestsFromName('__name__')
