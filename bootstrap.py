b8812b38c3526152f31c72f5baff17390382d4c8
Leonard Richardson <leonard.richardson@canonical.com>
2011-01-28 11:39:36 -0500

    Moved everything into the top-level directory and got rid of buildout.

--- bootstrap.py
+++
@@ -1,1 +0,0 @@
-_bootstrap/bootstrap.py
--- to3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir python3
-for i in BeautifulSoupTests.py builder.py element.py dammit.py
-do
-    cp $i python3/
-    2to3-3.0 -x next $i | patch -p0 python3/$i
-    cp python3/$i python3/$i.orig
-    patch -p0 python3/$i < $i.3.diff
-done
