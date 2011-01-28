4897d4c148c30cb751e5dbf76ed9004a5ed92a90
Leonard Richardson <leonard.richardson@canonical.com>
2011-01-28 13:30:40 -0500

    Refactored enough to get all of the tests to pass with test discovery, even though there are still some underlying problems.

--- tests/test_lxml.py
+++ tests/test_lxml.py
@@ -1,5 +1,6 @@
 """Tests to ensure that the lxml tree builder generates good trees."""
 
+from beautifulsoup.builder.lxml_builder import LXMLTreeBuilder
 from beautifulsoup.testing import (
     BuilderInvalidMarkupSmokeTest,
     BuilderSmokeTest,
@@ -7,11 +8,6 @@ from beautifulsoup.testing import (
 
 class TestLXMLBuilder(BuilderSmokeTest):
     """See `BuilderSmokeTest`."""
-
-    def test_bare_string(self):
-        # lxml puts a <p> tag around the bare string.
-        self.assertSoupEquals(
-            "A bare string", "<p>A bare string</p>")
 
     def test_foo(self):
         isolatin = """<html><meta http-equiv="Content-type" content="text/html; charset=ISO-Latin-1" />Sacr\xe9 bleu!</html>"""
