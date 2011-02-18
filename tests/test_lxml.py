68d23df73772cf15f942cde4e16c8fd157bfe356
Leonard Richardson <leonard.richardson@canonical.com>
2011-02-18 08:54:29 -0500

    Removed partially ported test that's now completely ported.

--- tests/test_lxml.py
+++ tests/test_lxml.py
@@ -304,14 +304,6 @@ class TestLXMLBuilder(SoupTest):
         str = soup.p.string
         #self.assertEquals(str.encode("utf-8"), expected)
 
-    def test_foo(self):
-        isolatin = """<html><meta http-equiv="Content-type" content="text/html; charset=ISO-Latin-1" />Sacr\xe9 bleu!</html>"""
-        soup = self.soup(isolatin)
-
-        utf8 = isolatin.replace("ISO-Latin-1".encode(), "utf-8".encode())
-        utf8 = utf8.replace("\xe9", "\xc3\xa9")
-        #print soup
-
 
 class TestLXMLBuilderInvalidMarkup(SoupTest):
     """Tests of invalid markup for the LXML tree builder.
